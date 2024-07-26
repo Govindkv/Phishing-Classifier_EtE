4173ccf5e5dec5fbe3cd37287e1b281e7dc75630
Govindkv <Govind26663355@gmail.com>
2024-07-26 15:23:37 +0530

    vizual code

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Phishing-Classifier_EtE
 This is an End to End ML project 1
+
+'''pip install -e . ''' to run setup.py
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Govindkv",
+    author_email="govind26663355@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
